a45ffee8640caa4d1957e8df8a0e4b9f9f1082bc
Otavio Fernandes <otaviof@gmail.com>
2022-06-14 09:36:15 +0200

    Python Project Bootstrap

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='shipwright_tests',
+    version='0.0.1',
+    packages=find_packages(include=['shipwrighttests', 'shipwrighttests.*']),
+	install_requires=[
+		'behave==1.2.6'
+	],
+)
