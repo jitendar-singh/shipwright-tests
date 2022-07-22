3dc285b8e947ab54ac1e0f3ac302bf4ac9a242a3
Jitendar Singh <jitsingh@redhat.com>
2022-07-22 15:16:56 +0530

    Refactoring the code

--- shipwrighttests/features/steps/install_steps.py
+++ shipwrighttests/features/steps/install_steps.py
@@ -10,28 +10,35 @@ def probe_cluster_available(context):
 	"""Probe if the cluster is available
 
 	Args:
-		context (_type_): It holds the contextual information during the execution of tests. 
-		It is an object that can store user-defined data along with Python Behave-defined data, in context attributes
+		context (_type_): It holds the contextual information during the execution of tests.
+		It is an object that can store user-defined data along with
+		Python Behave-defined data, in context attributes.
 
 	Raises:
 		config.ConfigException: When unable to load kubeconfig
-	"""	
-	try:
-		config.load_incluster_config()
-	except config.ConfigException:
-		try:
-			config.load_kube_config()
-			print("Active host is %s" % configuration.Configuration().host)
-		except config.ConfigException:
-			raise Exception("cluster probe failed")
+	"""
+	v1 = client.CoreV1Api()
+	node_state = {}
+	node_lst = v1.list_node()
+	for nodes in node_lst.items:
+		print("Evaluating node: ",nodes.metadata.name)
+		node_status = v1.read_node_status(nodes.metadata.name)
+		for status in node_status.status.conditions:
+			node_state[status.type] = status.status
+	if 'Ready' in node_state.keys():
+		if node_state['Ready'] == 'True':
+			print("INFO: Node ",nodes.metadata.name," status is: ",node_state['Ready'])
+		else:
+			raise Exception("ERR: Node ",nodes.metadata.name," status:",node_state['Ready'])
 
 @then(u'"{deployment}" is deployed and "{state}" on "{namespace}" namespace')
 def probe_deployment_state(context, deployment, state, namespace):
-	"""probe the state of deployments
+	"""Probe the state of deployments
 
 	Args:
-		context (_type_): It holds the contextual information during the execution of tests. 
-		It is an object that can store user-defined data along with Python Behave-defined data, in context attributes
+		context (_type_): It holds the contextual information during the execution of tests.
+		It is an object that can store user-defined data along with
+		Python Behave-defined data, in context attributes
 		deployment (str): name of the deployment
 		state (str): current state of the deployment
 		namespace (str): namespace in which the deployment is present
@@ -40,25 +47,28 @@ def probe_deployment_state(context, deployment, state, namespace):
 		Exception: Raises a exception when deployment is not ready
 
 	Returns:
-		bool: True based on deployment state
-	"""	
+			bool: True based on deployment state
+	"""
 	appsv1 = client.AppsV1Api()
 	deployment_state = appsv1.read_namespaced_deployment_status(deployment, namespace)
-	if deployment_state.status.unavailable_replicas is None:
-		return True
+	desired_replicas = deployment_state.spec.replicas
+	ready_replicas = deployment_state.status.ready_replicas
+	if desired_replicas == ready_replicas:
+		print("INFO: Deployment ",deployment," desired replicas:<",desired_replicas,"> ready replicas:<",ready_replicas,">")
 	else:
-		raise Exception("Deployment not ready")
+		raise Exception("ERR: Deployment ",deployment," desired replicas:<",desired_replicas,"> ready replicas:<",ready_replicas,">")
 		
 @then(u'the following resource names belong to "{api_version}" API version')
 def probe_resources_for_apiversion(context, api_version):
-	"""probe the scope of CustomResourceDefinitions
-	   i.e. Namespaced or Cluster
+	"""Probe the scope of CustomResourceDefinitions
+		i.e. Namespaced or Cluster
 
 	Args:
 		context (_type_): It holds the contextual information during the execution of tests.
-		 It is an object that can store user-defined data along with Python Behave-defined data, in context attributes
+			It is an object that can store user-defined data along with
+			Python Behave-defined data, in context attributes
 		api_version (_type_): api version of the CustomResourceDefinitions
-	"""	
+	"""
 	crd_scope = {}
 	cusobj = client.ApiextensionsV1Api()
 	res = cusobj.list_custom_resource_definition()
@@ -79,13 +89,16 @@ def probe_cluster_objects(context, name):
 	"""Probe the availability of custom objects created by CustomResourceDefinitions
 
 	Args:
-		context (_type_): It holds the contextual information during the execution of tests. 
-		It is an object that can store user-defined data along with Python Behave-defined data, in context attributes
+		context (_type_): It holds the contextual information during the execution of tests.
+		It is an object that can store user-defined data along with
+		Python Behave-defined data, in context attributes
 		name (str): name of the custom object
-	"""	
+	"""
+	cstobj = client.CustomObjectsApi()
 	model = getattr(context, "model", None)
 	if not model:
-		context.model = ResourcesModel()
+		context.model = ResourcesModel(crd_scope={},object_name="")
 	for row in context.table:
-		res = context.model.get_custom_objects(object_name=row["name"])
+		object_name = cstobj.get_cluster_custom_object('shipwright.io', 'v1alpha1', 'clusterbuildstrategies', row["name"])
+		res = context.model.get_custom_objects(object_name["metadata"]["name"])
 		assert_that(row["name"],equal_to(res))
--- shipwrighttests/features/steps/resources.py
+++ shipwrighttests/features/steps/resources.py
@@ -1,11 +1,10 @@
 from kubernetes import client
-
 class ResourcesModel(object):
-    def __init__(self,crd_scope={},object_name=None):
+    def __init__(self,crd_scope={},object_name=""):
         self.crd_scope = crd_scope
 
     def get_crd_scope(self, resource_name):
-        """This method acts an model method used\n
+        """This method acts an model method used
            to get the scope of CustomResourceDefinitions
         Args:
             resource_name (str): Name of the CustomResourceDefinitions
@@ -17,7 +16,7 @@ class ResourcesModel(object):
         return scope
     
     def get_custom_objects(self, object_name):
-        """This method acts an model method used\n
+        """This method acts an model method used
         to search the custom objects created by CustomResourceDefinitions
 
         Args:
@@ -25,8 +24,9 @@ class ResourcesModel(object):
 
         Returns:
             str: custom object
-        """        
+        """
+        self.object_name = object_name
         cstobj = client.CustomObjectsApi()
-        res = cstobj.get_cluster_custom_object('shipwright.io', 'v1alpha1', 'clusterbuildstrategies', object_name)
+        res = cstobj.get_cluster_custom_object('shipwright.io', 'v1alpha1', 'clusterbuildstrategies', self.object_name)
         return res["metadata"]["name"]
         
